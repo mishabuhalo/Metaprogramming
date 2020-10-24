0edfbc60e9542afef244b4c5ffb86b22eeaa5426
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-10-24 15:39:48 +0300

    added token to string method

--- CSSFormater/CSSFormater/Token.cs
+++ CSSFormater/CSSFormater/Token.cs
@@ -8,6 +8,11 @@ namespace CSSFormater
     {
         public TokenTypes TokenType { get; set; }
         public string TokenValue { get; set; }
+
+        public override string ToString()
+        {
+            return $"\"{TokenType.ToString("g")}\": \"{TokenValue}\"";
+        }
     }
 
    public enum TokenTypes
@@ -20,6 +25,6 @@ namespace CSSFormater
         Identifier,
         Number,
         Operator,
-        Bracket
+        Symbol
     }
 }
