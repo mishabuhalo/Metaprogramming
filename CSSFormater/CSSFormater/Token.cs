5497292959a2c0d72e13c3b64a0e7619d575c419
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-10-24 14:32:02 +0300

    added token class

---
+++ CSSFormater/CSSFormater/Token.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSSFormater
+{
+    public class Token
+    {
+        public TokenTypes TokenType { get; set; }
+        public string TokenValue { get; set; }
+    }
+
+   public enum TokenTypes
+    {
+        Comment,
+        String,
+        WhiteSpace,
+        Tab,
+        NewLine,
+        Identifier,
+        Number,
+        Operator,
+        Bracket
+    }
+}
