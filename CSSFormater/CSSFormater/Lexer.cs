cd2bd9387ac626e40aa8ca023f334c6e4598627c
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-10-24 14:44:37 +0300

    added next line and next character methods

--- CSSFormater/CSSFormater/Lexer.cs
+++ CSSFormater/CSSFormater/Lexer.cs
@@ -39,7 +39,59 @@ namespace CSSFormater
                 this.CurrentLineNumber = -1;
                 this.CurrentLine = "";
                 this.CurrentCharacter = '\0';
+
+                this.NextLine();
+                this.NextCharacter();
             }
+        }
+
+        private string NextLine()
+        {
+            var lexer = this;
+
+            lexer.CurrentLineNumber += 1;
+
+            if(lexer.TotalLinesCount <= lexer.CurrentLineNumber)
+            {
+                lexer.CurrentLine = String.Empty;
+            }
+            else
+            {
+                lexer.CurrentLine = lexer.Lines[lexer.CurrentLineNumber];
+            }
+            if(lexer.CurrentCharacterNumber!=-1)
+            {
+                lexer.CurrentCharacterNumber = 0;
+            }
+            return lexer.CurrentLine;
+        }
+
+        private char NextCharacter()
+        {
+            var lexer = this;
+
+            lexer.CurrentCharacterNumber += 1;
+
+            if(lexer.CurrentCharacterNumber == lexer.CurrentLine.Length)
+            {
+                if(String.IsNullOrEmpty(this.NextLine()))
+                {
+                    this.CurrentCharacter = '\0';
+                    return '\0';
+                }
+                this.CurrentCharacterNumber = -1;
+                this.CurrentCharacter = '\n';
+                return '\n';
+            }
+
+            this.CurrentCharacter = lexer.CurrentLine[lexer.CurrentCharacterNumber];
+
+            return this.CurrentCharacter;
+        }
+
+        private List<Token> Tokenize()
+        {
+            throw new NotImplementedException();
         }
     }
 }
