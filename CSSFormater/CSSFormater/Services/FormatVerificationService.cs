74d5677927d9d06049276eb3cce217307b34ed3f
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-10-24 22:59:08 +0300

    initial formater verification service

---
+++ CSSFormater/CSSFormater/Services/FormatVerificationService.cs
@@ -0,0 +1,16 @@
+﻿using CSSFormater.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSSFormater.Services
+{
+    public class FormatVerificationService
+    {
+        private List<VerificationError> verificationErrors;
+        public void VerifyFileTokens(List<Token> fileTokens)
+        {
+
+        }
+    }
+}
