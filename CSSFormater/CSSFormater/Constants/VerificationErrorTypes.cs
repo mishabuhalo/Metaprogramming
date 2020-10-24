4167a160ce193f1302199a499de5daca189a67ac
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-10-24 22:58:01 +0300

    added verification error types

---
+++ CSSFormater/CSSFormater/Constants/VerificationErrorTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSSFormater.Constants
+{
+    public static class VerificationErrorTypes
+    {
+        public const string TabsAndIndentsError = "Tabs and indents error";
+        public const string BlankLinesError = "Blank lines error";
+        public const string ArrangementError = "Arrangement error";
+        public const string BracesReplacementError = "Braces replacement error";
+        public const string AlignValuesError = "Align values error";
+        public const string QuoteMarksError = "Quote marks error";
+        public const string SpacesError = "Spaces error";
+        public const string HexColorsError = "HEX colors error";
+    }
+}
