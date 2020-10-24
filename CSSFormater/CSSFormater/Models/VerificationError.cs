c8017be7bfe27f7b69ce40d1f7a20454a423d89b
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-10-24 22:58:14 +0300

    added verification error model

---
+++ CSSFormater/CSSFormater/Models/VerificationError.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSSFormater.Models
+{
+    public class VerificationError
+    {
+        public string ErrorType { get; set; }
+        public string ErrorMessage { get; set; }
+        public int ErrorLineNumber { get; set; }
+        public int ErrorPosition { get; set; }
+    }
+}
