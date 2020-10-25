b55e06dbd6a69db82ea204ff23d81e14cbf6d17f
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-10-25 12:22:00 +0200

    added braces placement types

---
+++ CSSFormater/CSSFormater/Constants/BracesPlacementTypes.cs
@@ -0,0 +1,8 @@
+﻿namespace CSSFormater.Constants
+{
+    public static class BracesPlacementTypes
+    {
+        public const string NextLine = "Next line";
+        public const string EndOfLine = "End of line";
+    }
+}
