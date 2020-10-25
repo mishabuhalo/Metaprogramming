7f518051b5c89cb83de46f491963a2d3dec21d1a
Mykhailo Bukhalo <m.bukhalo@uds.systems>
2020-10-25 11:57:06 +0200

    added blank lines verification

--- CSSFormater/CSSFormater/Services/FormatVerificationService.cs
+++ CSSFormater/CSSFormater/Services/FormatVerificationService.cs
@@ -3,6 +3,7 @@ using CSSFormater.FormaterConfigurationModels;
 using CSSFormater.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CSSFormater.Services
@@ -20,6 +21,7 @@ namespace CSSFormater.Services
         public void VerifyFileTokens(List<Token> fileTokens)
         {
             TabsAndIndentsVerification(fileTokens);
+            BlankLinesValidation(fileTokens);
         }
 
         private void TabsAndIndentsVerification(List<Token> fileTokens)
@@ -95,6 +97,47 @@ namespace CSSFormater.Services
                         else
                             i++;
                     }
+                }
+            }
+        }
+
+        private void BlankLinesValidation(List<Token> fileTokens)
+        {
+            var maximumBlankLinesInCode = _configuration.BlankLines.MaximumBlankLinesInCode;
+            var minimumBlankLinesAroundTopLevelBlocks = _configuration.BlankLines.MinimumBlankLinesAroundTopLevelBlock;
+
+            var lastClosedBracketToken = fileTokens.Where(x => x.TokenValue == "}").Last();
+
+            var lastIndexOfClosingBracket = fileTokens.LastIndexOf(lastClosedBracketToken);
+
+            var countOfNextEmptyLines = 0;
+            for(int i = lastIndexOfClosingBracket; i < fileTokens.Count; ++i)
+            {
+                if (fileTokens[i].TokenType == TokenTypes.NewLine)
+                    countOfNextEmptyLines++;
+            }
+            if (maximumBlankLinesInCode < countOfNextEmptyLines - 1)
+                verificationErrors.Add(new VerificationError { ErrorLineNumber = lastClosedBracketToken.LineNumber + 1, ErrorMessage = $"Code style error. You exceeded the limit of extra blank lines to be kept on {countOfNextEmptyLines - maximumBlankLinesInCode}.", ErrorType = VerificationErrorTypes.BlankLinesError });
+
+            var j = 0;
+            for(; j< fileTokens.Count-1; ++j)
+            {
+                if(fileTokens[j].TokenValue == "}" && j!= lastIndexOfClosingBracket)
+                {
+                    j++;
+                    var lineNumber = fileTokens[j + 1].LineNumber;
+                    var currentBlankLinesCount = 0;
+                    while(fileTokens[j].TokenValue!="{" && j < fileTokens.Count)
+                    {
+                        if(fileTokens[j].TokenType == TokenTypes.NewLine)
+                        {
+                            currentBlankLinesCount++;
+                        }
+                        j++;
+                    }
+
+                    if (currentBlankLinesCount - 1 < minimumBlankLinesAroundTopLevelBlocks)
+                        verificationErrors.Add(new VerificationError { ErrorLineNumber = lineNumber, ErrorMessage = $"Code style error. You ddid not reach the minimum number of blank lines around top level blocks on {minimumBlankLinesAroundTopLevelBlocks - currentBlankLinesCount+1}.", ErrorType = VerificationErrorTypes.BlankLinesError });
                 }
             }
         }
